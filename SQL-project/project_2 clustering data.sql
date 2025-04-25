7a2602c27538b8b2cbe33a6e12d2fb7b9a07336a
Wagnew Moges <149929587+wagnewAI@users.noreply.github.com>
2025-04-25 20:02:32 +0300

    Update project_2 clustering data.sql

--- SQL-project/project_2 clustering data.sql
+++ SQL-project/project_2 clustering data.sql
@@ -352,3 +352,17 @@ hour_of_day
 ORDER BY
 hour_of_day; 
 # In the bar gragh, The colors represent the hours of the day, and each bar is the average queue time, for that specific hour and day
+## Water Accessibility and infrastructure summary report
+This survey aimed to identify the water sources people use and determine both the total and average number of users for each source.
+Additionally, it examined the duration citizens typically spend in queues to access water.
+	#Insights
+1. Most water sources are rural.
+2. 43% of our people are using shared taps. 2000 people often share one tap.
+3. 31% of our population has water infrastructure in their homes, but within that group, 45% face non-functional systems due to issues with pipes,
+pumps, and reservoirs.
+4. 18% of our people are using wells of which, but within that, only 28% are clean..
+5. Our citizens often face long wait times for water, averaging more than 120 minutes.
+6. In terms of queues:
+- Queues are very long on Saturdays.
+- Queues are longer in the mornings and evenings.
+- Wednesdays and Sundays have the shortest queues.
